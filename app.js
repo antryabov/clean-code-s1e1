57c88e61df4bd2ad30b1fd566cacdc2352141aca
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-11-12 12:46:52 +0300

    refactor(extended-1.2): add media alternatives

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="remove-img";
+  deleteButtonImg.alt="remove task button";
   deleteButton.appendChild(deleteButtonImg);
 
 
